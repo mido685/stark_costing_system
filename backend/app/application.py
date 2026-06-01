e6790dcc3090038a2e437dfa74714a88d4ce5477
mohamed stark <midokhodairy011181@gmail.com>
2026-06-02 01:49:25 +0300

    feat: your change description

--- backend/app/application.py
+++ backend/app/application.py
@@ -15,7 +15,7 @@ def create_app() -> FastAPI:
 
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=['*'],
+        allow_origins=["https://stark-costing-system.vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
